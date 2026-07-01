91f99b236a49b1c1c6884eca68a6dafdf7e9997c
Roman <maisonik120@gmail.com>
2026-07-01 17:23:00 +0300

    added movement physics and observer angle

--- Assets/LocalPlayerMovement.cs
+++ Assets/LocalPlayerMovement.cs
@@ -1,6 +1,75 @@
 using UnityEngine;
-
-public class NewEmptyCSharpScript
+using UnityEngine.InputSystem;
+public class LocalPlayerMovement : MonoBehaviour
 {
-    
+    [Header("найстройки движения")]
+    [SerializeField] private float MaxSpeed = 5f;
+    [SerializeField] private float acceleration = 25f;
+    [SerializeField] private float deceleration = 20f;
+
+    private Rigidbody2D rb;
+    private Camera mainCamera;
+
+    private Vector2 inputVector;
+    private Vector2 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        float MoveX = 0f;
+        float MoveY = 0f;
+        
+        if (Keyboard.current != null)
+        {
+            if (Keyboard.current.wKey.isPressed) MoveY = 1f;
+            if (Keyboard.current.sKey.isPressed) MoveY = -1f;
+            if (Keyboard.current.aKey.isPressed) MoveX = -1f;
+            if (Keyboard.current.dKey.isPressed) MoveX = 1f;
+        }
+
+        inputVector = new Vector2(MoveX, MoveY).normalized;
+        RotateTowardsMouse();
+
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 targetVelocity = inputVector * MaxSpeed;
+        float currentAcceleration = (inputVector.magnitude > 0) ? acceleration : deceleration;
+        velocity = Vector2.MoveTowards(velocity, targetVelocity, currentAcceleration * Time.fixedDeltaTime);
+        rb.linearVelocity = velocity;
+    }
+
+    private void RotateTowardsMouse()
+    {
+        // Проверяем, подключена ли мышь, чтобы не было ошибок
+        if (Mouse.current == null) return;
+
+        // Считываем позицию мыши ТОЛЬКО через Новый Input System
+        Vector3 mouseScreenPosition = Mouse.current.position.ReadValue();
+
+        // Задаем дистанцию до камеры в 2D пространстве
+        mouseScreenPosition.z = Mathf.Abs(mainCamera.transform.position.z);
+
+        // Переводим пиксели экрана в координаты игрового мира
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+
+        // Считаем угол и плавно поворачиваем физическое тело
+        Vector2 lookDirection = (mouseWorldPosition - transform.position).normalized;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+        rb.MoveRotation(angle);
+    }
 }
+
+
